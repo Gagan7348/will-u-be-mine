c119d85753d909459d8e978a8ebe7eaeceae4d86
Gagan7348 <tulsimahima111@gmail.com>
2026-02-14 03:54:46 +0530

    Add all project files

---
+++ src/components/LastPage.tsx
@@ -0,0 +1,18 @@
+import CuteHeart from "./CuteHeart";
+import { CONFIG } from "../config";
+const LastPage = () => {
+  return (
+    <div className="flex flex-col items-center gap-8 p-4 text-center">
+      <h2 className="text-3xl font-bold text-primary">With All My Love</h2>
+      <p className="text-lg max-w-2xl">
+        Thank you for being with me, for turning ordinary moments into
+        unforgettable memories, and for lighting up my world every day. Your
+        love fills my heart and inspires me to be the best version of myself.
+        I’m forever grateful to have you by my side. With love - gagan
+      </p>
+      <CuteHeart />
+    </div>
+  );
+};
+
+export default LastPage;
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    yourName: "gagan",
+    recipientName: "avani",
+};
